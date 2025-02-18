564aa453b2929275f7893ea113b27fc431c8ef50
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-18 17:45:52 +0100

    Added task info and test.mjs

--- Tasks/poppingFunctions.mjs
+++ Tasks/poppingFunctions.mjs
@@ -1,4 +1,13 @@
-
+//#region 
+/*## Task : Functions are a popping
+Create the following functions: 
+- A function that returns the square of a number
+- A function that returns a length in mm assuming it has been given a length in inches.
+- A function that returns the root of a number
+- A function that returns the cube of a number 
+- A function that returns the area of a circle given the radius. 
+- A function that returns a greeting, given a name.*/
+//#endregion
 function square(num) {
     return num * num;
 }
