2ce294c57f3f1f489318da34d6f2234dccd168c2
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-18 17:29:57 +0100

    added files to repo, and created intial functions

---
+++ Tasks/poppingFunctions.mjs
@@ -0,0 +1,27 @@
+
+function square(num) {
+    return num * num;
+}
+
+function inchToMM(int) {
+    const inchInMm = 25.4;
+    return int * inchInMm;
+}
+
+function root(){
+
+}
+
+function cube(num) {
+    return num ** num;
+}
+
+function areaOfCircle(radius) {
+    const PI = 3.14159;
+    return PI * radius * radius;
+}
+
+function greet(name) {
+    return `Greetings, ${name}!`
+}
+
