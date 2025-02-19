6dc60b74c06bbc4dd2810f9ba5e04b5635e92148
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-19 01:35:46 +0100

    removed magic numbers and started on root function

--- Tasks/poppingFunctions.mjs
+++ Tasks/poppingFunctions.mjs
@@ -8,29 +8,47 @@ Create the following functions:
 - A function that returns the area of a circle given the radius. 
 - A function that returns a greeting, given a name.*/
 //#endregion
+
+const consts = {
+    INCH_CONVERSION: 25.4,
+    PI: 3.14159,
+    DEFAULT_PRECISION: 1e-7,
+    ERROR_NEGATIVE_ROOT: "Can't root negative numbers",
+    GREETING_TEMP: "Greetings, "
+}
+
 function square(num) {
     return num * num;
 }
 
 function inchToMM(int) {
-    const inchInMm = 25.4;
-    return int * inchInMm;
+    return int * consts.INCH_CONVERSION;
 }
 
-function root(){
+function root(num){
 
+    if (num < 0) {
+        throw new Error(consts.ERROR_NEGATIVE_ROOT); 
+    }
+
+    let x = num, prev;
+    do {
+        prev = x;
+        x = (x + n / x)
+    }
 }
 
 function cube(num) {
-    return num ** num;
+    return num * num * num;
 }
 
 function areaOfCircle(radius) {
-    const PI = 3.14159;
-    return PI * radius * radius;
+    return consts.PI * radius * radius;
 }
 
 function greet(name) {
-    return `Greetings, ${name}!`
+    return consts.GREETING_TEMP `${name}!`;
 }
 
+console.log(cube(3));
+console.log(greet("Aleks"));
