d31caedc305d98139a4879d2a26b9476376a18eb
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-21 00:30:29 +0100

    Forgot to commit as I was in the zone. Stuff done: Made root function pure. Added functionality tests. Added testing area with some tests for square function. Added some comments for clarification. Added util functions for console readability. Imported test.mjs. Fixed some issues with the functions, e.g. absolute had no return value.

--- Tasks/poppingFunctions.mjs
+++ Tasks/poppingFunctions.mjs
@@ -1,4 +1,6 @@
-//#region 
+import test from "../test.mjs";
+
+//#region Tasks
 /*## Task : Functions are a popping
 Create the following functions: 
 - A function that returns the square of a number
@@ -12,9 +14,13 @@ Create the following functions:
 const consts = {
     INCH_CONVERSION: 25.4,
     PI: 3.14159,
-    DEFAULT_PRECISION: 1e-7,
+    DEFAULT_PRECISION: 1e-7, //Set to 1e-7 following Newton-Raphson method
     ERROR_NEGATIVE_ROOT: "Can't root negative numbers",
     GREETING_TEMP: "Greetings, "
+}
+
+function absolute(value) { // Helper function to ensure input value isn't negative used in later function
+    return value < 0 ? -value : value; 
 }
 
 function square(num) {
@@ -25,27 +31,23 @@ function inchToMM(int) {
     return int * consts.INCH_CONVERSION;
 }
 
-function absolute(value) {
-    value < 0 ? -value : value; 
-}
-
 function root(num, precision = consts.DEFAULT_PRECISION){
 
     if (num < 0) {
-        throw new Error(consts.ERROR_NEGATIVE_ROOT); 
+        return false;
     }
 
     let x = num, prev;
     do {
         prev = x;
-        x = (x + n / x)
+        x = (x + num / x) / 2;
     } while (absolute(x - prev) > precision);
 
     return x;
 }
 
 function cube(num) {
-    return num * num * num;
+    return num * square(num);
 }
 
 function areaOfCircle(radius) {
@@ -56,5 +58,39 @@ function greet(name) {
     return `${consts.GREETING_TEMP}${name}!`;
 }
 
-console.log(cube(3));
-console.log(greet("Aleks"));
+addSpacing(1);
+
+
+// Use-case Functionality Tests
+
+print(square(10));
+print(inchToMM(20));
+print(root(16));
+print(cube(4));
+print(areaOfCircle(5));
+print(greet("Aleks"));
+
+addSpacing(1);
+
+// Logical Tests
+
+const tester = test("Function Test");
+
+tester.isEqual(square(5), 25, "Square of 5 | Positive Numbers");
+tester.isEqual(square(-3), 9, "Square of -3 | Negative numbers");
+tester.isEqual(square(0), 0, "Square of 0 | Squaring 0");
+tester.isEqual(square(2.5), 6.25, "Square of 2.5 | Decimal numbers");
+
+//#region Util Functions
+
+function print(text) {
+    console.log(text);
+}
+
+function addSpacing(lines = 1) {
+    for (let i = 0; i < lines; i++) {
+        console.log("");
+    }
+}
+
+//#endregion
