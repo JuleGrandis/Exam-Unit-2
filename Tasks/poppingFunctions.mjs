820001aa720ff0b02bd976fc2dcc44bae009ede8
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-22 00:04:25 +0100

    removed test import on tasks workspace. Added final tests on testTask1.mjs. Finished Task 1!

--- Tasks/poppingFunctions.mjs
+++ Tasks/poppingFunctions.mjs
@@ -1,4 +1,3 @@
-import test from "../test.mjs";
 
 //#region Tasks
 /*## Task : Functions are a popping
--- testing_tasks/testTask1.mjs
+++ testing_tasks/testTask1.mjs
@@ -19,6 +19,7 @@ tester.isNotANumber(square(undefined), "Square of undefined should be NaN | Test
 tester.isNotANumber(square("Wizard"), "Squaring 'Wizard' should return NaN | Testing string");
 
 addSpacing(1);
+
 // inchToMM Function ------------------------------------------------------------------------------------------------
 tester.isEqual(inchToMM(20), 508, "Converting 20 inches to mm, should return 508 | Testing positive numbers");
 tester.isEqual(inchToMM(0), 0, "Converting 0 inches to mm, should return 0 | Testing with 0");
@@ -31,6 +32,7 @@ tester.isNotANumber(inchToMM(undefined), "Testing with 'undefined' input, should
 tester.isNotANumber(inchToMM("Staff"), "Testing with string, should return NaN");
 
 addSpacing(1);
+
 // root Function ----------------------------------------------------------------------------------------------------
 tester.isEqual(root(16), 4, "Root of 16, should return 4. | Testing with positive numbers.");
 tester.isEqual(root(0), 0, "Root of 0, should rerun 0. | Testing with 0.");
@@ -45,6 +47,7 @@ tester.dosNotThrowError(() => root(undefined), "Root of undefined, should throw 
 tester.dosNotThrowError(() => root("Wonderful"), "Root of string, should throw error. | string error handling.");
 
 addSpacing(1);
+
 // cube Function ------------------------------------------------------------------------------------------------------
 tester.isEqual(cube(4), 64, "Cube of 4, should return 64 | Testing positive numbers.");
 tester.isEqual(cube(0), 0, "Cube of 0, should return 0 | Testing with 0");
@@ -57,8 +60,32 @@ tester.isNotANumber(cube(null), "Cube of null, should return NaN | NaN handling"
 tester.isNotANumber(cube(undefined), "Cube of undefined, should return NaN | NaN handling");
 tester.isNotANumber(cube("Pokemon"), "Cube of string, should return NaN | NaN handling");
 
-// areaOfCircle function -----------------------------------------------------------------------------------------------
+addSpacing(1);
 
+// areaOfCircle function -----------------------------------------------------------------------------------------------
+tester.isEqual(areaOfCircle(5), 78.53975, "Area of circle with R = 5, should return 78.53975 | Testing with positive numbers");
+tester.isEqual(areaOfCircle(0), 0, "Area of circle with R = 0, should return 0 | Testing with 0");
+tester.isInRange(areaOfCircle(1.5), 7.0685774, 7.0685775, "Area of circle with R = 1.5, should return ∼7.0685775");
+tester.dosNotThrowError(() => areaOfCircle(-5), "Area of circle with R = -5, should throw an error. | Testing negative number error handling.");
+
+// Edge cases
+tester.isInRange(areaOfCircle(0.0001), 0, 1e-7, "Area of circle with R = 0.0001, should return value between 0 and 1e-7 | Testing small numbers.");
+tester.isEqual(areaOfCircle(1e10), 3.14159e20, "Area of 1e10, should return 3.14159e20 | Testing large numbers.");
+tester.dosNotThrowError(() => areaOfCircle(null), "Area of 'null', should throw an error | Testing with null.");
+tester.dosNotThrowError(() => areaOfCircle(undefined), "Area of 'undefined', should throw an error | Testing with undefined.");
+tester.dosNotThrowError(() => areaOfCircle("Charizard"), "Area of string, should throw an error | Testing with string");
+
+addSpacing(1);
+
+// greet function ------------------------------------------------------------------------------------------------------
+tester.isEqual(greet("Aleksander"), "Greetings, Aleksander!", "Greeting myself | Testing with regular input.");
+tester.isEqual(greet(""), "Greetings, !", "Greeting empty string | Testing with empty string");
+
+// Edge cases
+tester.isEqual(greet(null), "Greetings, null!", "Greeting 'null' | Testing with 'null'");
+tester.isEqual(greet(undefined), "Greetings, undefined!", "Greeting 'undefined' | Testing with 'undefined'");
+tester.isEqual(greet(7), "Greetings, 7!", "Greeting 7 | Testing with numbers");
+tester.isEqual(greet("X".repeat(2000)), `Greetings, ${"X".repeat(2000)}!`, "Greeting X(...) | Testing with long names");
 //#region Util Functions
 
 function print(text) {
