38f4339bbf9ceeb66c2a287e670fa7a1cd00f3ee
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2025-02-21 15:40:12 +0100

    Removed util functions, not needed

--- Tasks/poppingFunctions.mjs
+++ Tasks/poppingFunctions.mjs
@@ -81,17 +81,3 @@ export function mathOperations(operation) {
 export const square = mathOperations("square");
 export const cube = mathOperations("cube");
 export const root = mathOperations("root");
-
-//#region Util Functions
-
-function print(text) {
-    console.log(text);
-}
-
-function addSpacing(lines = 1) {
-    for (let i = 0; i < lines; i++) {
-        console.log("");
-    }
-}
-
-//#endregion
